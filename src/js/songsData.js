a6041748232b334cfcfdfced266c81e6d232fd2f
lftys2021 <88808275+lftys2021@users.noreply.github.com>
2026-05-21 07:25:44 +0900

    third commit

--- src/js/songsData.js
+++ src/js/songsData.js
@@ -1,61 +1,120 @@
 // songsData.js
 export const BUILTIN_SONGS = {
   airplane: {
-    title: "비행기 (전곡)",
-    bpm: 110,                // 조금 더 경쾌하게 속도를 올렸습니다.
-    timeSignature: "4/4",
-    totalMeasures: 8,        // 총 8소절 전곡 플레이
-    // 소절(Measure) 단위 배열 구조 유지
+    id: 'airplane',
+    title: '비행기 (전곡)',
+    bpm: 110,
+    timeSignature: '4/4',
+    totalMeasures: 8,
     measures: [
-      // 1 소절: 미-레-도-레
+      // 1마디: 미(4) 레(4) 도(4) 레(4)
       [
-        { tick: 0, note: "E5" },
-        { tick: 4, note: "D5" },
-        { tick: 8, note: "C5" },
-        { tick: 12, note: "D5" }
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'D5' },
+        { tick: 8, note: 'C5' }, { tick: 12, note: 'D5' }
       ],
-      // 2 소절: 미-미-미
+      // 2마디: 미(4) 미(4) 미-(8) <- 마지막 미는 2박자 롱노트 공간 유지
       [
-        { tick: 0, note: "E5" },
-        { tick: 4, note: "E5" },
-        { tick: 8, note: "E5" }
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'E5' },
+        { tick: 8, note: 'E5' }
       ],
-      // 3 소절: 레-레-레
+      // 3마디: 레(4) レ(4) 레-(8)
       [
-        { tick: 0, note: "D5" },
-        { tick: 4, note: "D5" },
-        { tick: 8, note: "D5" }
+        { tick: 0, note: 'D5' }, { tick: 4, note: 'D5' },
+        { tick: 8, note: 'D5' }
       ],
-      // 4 소절: 미-솔-솔
+      // 4마디: 미(4) 솔(4) 솔-(8)
       [
-        { tick: 0, note: "E5" },
-        { tick: 4, note: "G5" },
-        { tick: 8, note: "G5" }
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'G5' },
+        { tick: 8, note: 'G5' }
       ],
-      // 5 소절: 미-레-도-레 (2절 시작)
+      // 5마디: 미(4) 레(4) 도(4) 레(4)
       [
-        { tick: 0, note: "E5" },
-        { tick: 4, note: "D5" },
-        { tick: 8, note: "C5" },
-        { tick: 12, note: "D5" }
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'D5' },
+        { tick: 8, note: 'C5' }, { tick: 12, note: 'D5' }
       ],
-      // 6 소절: 미-미-미-미
+      // 6마디: 미(4) 미(4) 미-(8)
       [
-        { tick: 0, note: "E5" },
-        { tick: 4, note: "E5" },
-        { tick: 8, note: "E5" },
-        { tick: 12, note: "E5" }
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'E5' },
+        { tick: 8, note: 'E5' }
       ],
-      // 7 소절: 레-레-미-레
+      // 7마디: 레(4) 레(4) 미(4) 레(4)
       [
-        { tick: 0, note: "D5" },
-        { tick: 4, note: "D5" },
-        { tick: 8, note: "E5" },
-        { tick: 12, note: "D5" }
+        { tick: 0, note: 'D5' }, { tick: 4, note: 'D5' },
+        { tick: 8, note: 'E5' }, { tick: 12, note: 'D5' }
       ],
-      // 8 소절: 도--- (마무리)
+      // 8마디: 도---(16) <- 대단원의 마지막 롱노트
       [
-        { tick: 0, note: "C5" }
+        { tick: 0, note: 'C5' }
+      ]
+    ]
+  },
+
+  twinkleStar: {
+    id: 'twinkleStar',
+    title: '작은 별',
+    bpm: 96, // 조금 더 아늑하고 클래식한 오르골 템포
+    timeSignature: '4/4',
+    totalMeasures: 12,
+    measures: [
+      // 1마디: 도(4) 도(4) 솔(4) 솔(4) [반짝 반짝]
+      [
+        { tick: 0, note: 'C5' }, { tick: 4, note: 'C5' },
+        { tick: 8, note: 'G5' }, { tick: 12, note: 'G5' }
+      ],
+      // 2마디: 라(4) 라(4) 솔-(8) [작은 별]
+      [
+        { tick: 0, note: 'A5' }, { tick: 4, note: 'A5' },
+        { tick: 8, note: 'G5' }
+      ],
+      // 3마디: 파(4) 파(4) 미(4) 미(4) [아름답게]
+      [
+        { tick: 0, note: 'F5' }, { tick: 4, note: 'F5' },
+        { tick: 8, note: 'E5' }, { tick: 12, note: 'E5' }
+      ],
+      // 4마디: 레(4) 레(4) 도-(8) [비치네]
+      [
+        { tick: 0, note: 'D5' }, { tick: 4, note: 'D5' },
+        { tick: 8, note: 'C5' }
+      ],
+      // 5마디: 솔(4) 솔(4) 파(4) 파(4) [동쪽 하늘]
+      [
+        { tick: 0, note: 'G5' }, { tick: 4, note: 'G5' },
+        { tick: 8, note: 'F5' }, { tick: 12, note: 'F5' }
+      ],
+      // 6마디: 미(4) 미(4) 레-(8) [에서도]
+      [
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'E5' },
+        { tick: 8, note: 'D5' }
+      ],
+      // 7마디: 솔(4) 솔(4) 파(4) 파(4) [서쪽 하늘]
+      [
+        { tick: 0, note: 'G5' }, { tick: 4, note: 'G5' },
+        { tick: 8, note: 'F5' }, { tick: 12, note: 'F5' }
+      ],
+      // 8마디: 미(4) 미(4) 레-(8) [에서도]
+      [
+        { tick: 0, note: 'E5' }, { tick: 4, note: 'E5' },
+        { tick: 8, note: 'D5' }
+      ],
+      // 9마디: 도(4) 도(4) 솔(4) 솔(4) [반짝 반짝]
+      [
+        { tick: 0, note: 'C5' }, { tick: 4, note: 'C5' },
+        { tick: 8, note: 'G5' }, { tick: 12, note: 'G5' }
+      ],
+      // 10마디: 라(4) 라(4) 솔-(8) [작은 별]
+      [
+        { tick: 0, note: 'A5' }, { tick: 4, note: 'A5' },
+        { tick: 8, note: 'G5' }
+      ],
+      // 11마디: 파(4) 파(4) 미(4) 미(4) [아름답게]
+      [
+        { tick: 0, note: 'F5' }, { tick: 4, note: 'F5' },
+        { tick: 8, note: 'E5' }, { tick: 12, note: 'E5' }
+      ],
+      // 12마디: 레(4) 레(4) 도-(8) [비치네]
+      [
+        { tick: 0, note: 'D5' }, { tick: 4, note: 'D5' },
+        { tick: 8, note: 'C5' }
       ]
     ]
   }
